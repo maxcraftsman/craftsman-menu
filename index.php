b2bd406a1fc2a2c280d2f2ea3a6cfa07681e1c16
Maxim Tatarinov <maxcraftsman@users.noreply.github.com>
2017-05-18 15:40:39 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <script
+      src="https://code.jquery.com/jquery-2.2.4.min.js"
+      integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+      crossorigin="anonymous"></script>
+    <script src="main.js"></script>
+  </head>
+  <body>
+    <ul class="main">
+      <li>1.Main Page</li>
+      <li>2.Second Page</li>
+      <li>3.Third Page</li>
+      <li>4.Awesome Link</li>
+      <li>5.Click Here</li>
+      <li>6.Don't Click Here</li>
+      <li>7.Usefull Stuff</li>
+      <li>8.Bless You</li>
+      <li>9.Get Many Money</li>
+      <li>10.I don't think so</li>
+      <li>11.You can do it</li>
+      <li>12.Just do it</li>
+      <li>13.Hello</li>
+      <li>14.Longest Text Here</li>
+      <li>15.Meow</li>
+      <li>16.Okay</li>
+      <li>17.Google</li>
+      <li>18.Greatness</li>
+      <li>19.Light</li>
+      <li>20.Quick</li>
+    </ul>
+    <div class="more">More...
+      <div class="dropdown">
+        <ul>
+
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ main.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+  function toDropdownDown( jQuery ) {
+    $(".main>li").each(function(){
+      if ($(this).position().top == 58){
+        $(this).appendTo($('.dropdown ul'));
+      }
+    });
+  }
+  function toDropdownUp( jQuery ) {
+    $(".main>li").each(function(){
+      if ($(this).position().top == 58){
+        $(this).prependTo($('.dropdown ul'));
+      }
+    });
+  }
+  function toDropdownPlus(Jquery) {
+    var i = 0;
+    var width = 0;
+    var more = $(".more").offset().left;
+    var menu = $(".main li:nth-last-child(-2n+2)").offset().left;
+    var menuli = $(".main li:nth-last-child(-2n+2)").outerWidth();
+    var dist = more - menu - menuli - 200;
+    var dropdownli = $(".dropdown li:first-child").outerWidth();
+    $(".dropdown li").each(function(){
+      i++;
+      width = $(this).outerWidth() + width;
+      if (width <= dist){
+        $(".dropdown li:nth-child("+i+")").detach().appendTo(".main");
+      }
+    })
+    console.log(width);
+    console.log(dist);
+  }
+  function fromDropdown() {
+    $(".dropdown li").each();
+  }
+  resize = $('html').width();
+  $(window).on("resize", function() {
+    var action='';
+    var ulWidth=$('html').width();
+    if(ulWidth<resize) {
+        action = '-';
+        toDropdownUp();
+    } else if (ulWidth>resize) {
+        action = '+';
+        toDropdownPlus();
+    };
+    resize=ulWidth;
+    if(action=='-') {
+      console.log("minus");
+    } else if(action=='+') {
+      console.log("plus")
+    }
+
+  }).resize();
+  toDropdownDown();
+  $('.droped').appendTo('.dropdown ul');
+  function toMenu( jQuery ) {
+    var i = 0;
+    var totalWidth = 0;
+    $(".main li:not(:last)").each(function(){
+      totalWidth = totalWidth + $(this).width();
+      i++;
+    });
+    console.log(totalWidth);
+  };
+  toMenu();
+});
